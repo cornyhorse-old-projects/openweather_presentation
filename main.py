f28b78f7ee92c4a408225b6ecd6789b6e70c460f
Matt Kingsbury <kingsbury.matthew@gmail.com>
2019-08-06 18:28:51 -0600

    Working on adding database support

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from database import tests as db_test
 from module_owm import tests as pyowm_test
 from requests_owm import tests as request_test
-
+from database import database as db
+from database import models as m
 
 if __name__ == "__main__":
-    print("Hello World!")
-
+    db_test.test_all()
+    engine, session = db.sqlalchemy_connect()
+    m.recreate_models(engine)
+    session.commit()
 
