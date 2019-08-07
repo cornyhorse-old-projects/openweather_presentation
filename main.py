ccb59129b5654eef38196f3c62c08283e0bb15eb
Matt Kingsbury <kingsbury.matthew@gmail.com>
2019-08-06 21:45:38 -0600

    Added database synchronization

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from database import tests as db_test
-from module_owm import tests as pyowm_test
+from module_owm import tests as pyowm_test, weather as w
 from requests_owm import tests as request_test
 from database import database as db
 from database import models as m
 
 if __name__ == "__main__":
-    db_test.test_all()
+    # db_test.test_all()
     engine, session = db.sqlalchemy_connect()
-    m.recreate_models(engine)
+    m.recreate_models(engine, session)
     session.commit()
 
+    w.save_utah_weather(engine, session)
